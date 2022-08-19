c3e26bfbecc02a48765dbc9a604140563db49746
Christopher Bryan Suarez Mancilla <mancillabryan1996@outlook.com>
2022-08-18 20:43:27 -0500

    fininish lit init

--- components/dw-message.js
+++ components/dw-message.js
@@ -1,1 +1,14 @@
-import {LitElement,html}  from 'lit'
+import {LitElement,html,css}  from 'lit'
+
+class DwMessage extends LitElement{
+    static styles = css`
+        div{
+            background-color: #fcc;
+        }
+    `
+    render(){
+        return html`<div> hola Lit<div>`
+    }
+}
+
+customElements.define('dw-message', DwMessage)
